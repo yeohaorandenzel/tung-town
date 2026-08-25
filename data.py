da93075f3a7194439dc9ff56c462322694eb618f
jingying <tam.jing.ying_2026@nyjc.edu.sg>
2026-08-25 07:03:43 +0000

    data

--- data.py
+++ data.py
@@ -10,7 +10,16 @@ class data:
             self.exits: Dict[str, "Room"] = {}
             self.creatures: List["Creature"] = []
             self.__max_health = max_health
+    
+    class Weapon:
+        def __init__(self):
+            self.name = "barehand" #default weapon
+            self.damage = 1 #barehanded damage
 
+    class Inventory:
+        def __init__(self):
+            self.cap = 5 #maximum capacity of inventory
+            self.items = [] #list of all items in inventory
 
 tung_town_case = Room(
     "TownSquare",
@@ -20,3 +29,8 @@ tung_town_case = Room(
 
 scarlet_swamp = Room(
     "ScarletSwamp",
+
+current_location = "Tung Town Square"
+current_turn = player
+inventory.number (returns number of items in inventory)
+inventory.items (returns a list of items in inventory)

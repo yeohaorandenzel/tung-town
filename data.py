0e38da1e63b19376e2b7b0bdfce025e4c6b29760
yeohaorandenzel <yeodenzel@gmail.com>
2026-08-25 16:10:37 +0800

    fixes

--- data.py
+++ data.py
@@ -1,22 +1,48 @@
-class data:
+class ActionType:
+    MOVE = 1
+    START_BATTLE = 2
+    DISPLAY_DIA = 3
 
-    class Player:
-        def __init__(self, name: str, )
+class Action:
+    def __init__(self,type:ActionType,val):
+        self.type = type
+        self.val = val
 
-    class Room:
-        def __init__(self, name: str, description: str, max_health: int):
-            self.name = name
-            self.description = description
-            self.exits: Dict[str, "Room"] = {}
-            self.creatures: List["Creature"] = []
-            self.__max_health = max_health
+class Choice:
+    def __init__(self,desc,actions):
+        self.desc = desc
+        self.actions = actions
+class Creature:
+    pass
+class Room:
+    def __init__(self, name: str, description: str, max_health: int,exits: dict[str, "Room"]):
+        self.name = name
+        self.description = description
+        self.exits: dict[str, str] = exits
+        self.creatures: list[Creature] = []
+        self.__max_health = max_health
 
-
+class Player:
+    def __init__(self, name: str,current_room):
+        self.name = name
+        self.current_room = current_room
+def prompt_player_choice(choices_dict):
+    print(choices_dict["description"])
+    for choice_name,choice in choices_dict["choices"].items():
+        print(choice_name,choice.desc)
+    return input(">")
+def display(k):
+    pass
 tung_town_case = Room(
     "TownSquare",
     "A desolate town square surrounded by faded grass. A demolished statue of tung tung tung sahur stands at the centre, heavy with strange fruit. Two rectangular creatures stand next to you.",
-    100
+    100,
+    {"north": Choice("Scarlet Swamp", [Action(ActionType.MOVE, "scarlet_swamp")])}
 )
 
-scarlet_swamp = Room(
-    "ScarletSwamp",
+scarlet_swamp = Room("ScarletSwamp","Swampy",100,{"south": Choice("Tung town",[Action(ActionType.MOVE, "tung_town_case")])})
+
+room_dict = {
+    "tung_town_case": tung_town_case,
+    "scarlet_swamp": scarlet_swamp
+}
--- game.py
+++ game.py
@@ -4,31 +4,52 @@ class Battle:
         self.player = player
         self.enemy = enemy
         print("You engage "+ self.enemy.name)
+
+    def get_options(self):
+        
+        options = {
+            "choices": None
+        }
 class Game:
-    def __init__(self, maze):
+    def __init__(self):
         self.player = None
-        self.maze = maze
         self.battle = None
-    def welcome():
+        self.last_options = None
+    def is_gameover(self):
+        return False
+    def welcome(self):
         print("Hallo lets play da tung town press any key to start")
         input()
     
     def add_player(self, player):
         self.player = player
-
+    
+    def get_actions(self, choice_name):
+        return self.last_options["choices"][choice_name].actions
+    
     def get_options(self):
-        return self.player.current_room.get_options()
+        if self.battle:
+            options = self.battle.get_options()
+        else:
+            current_room = self.player.current_room
+            options = {
+                "description": current_room.description,
+                "choices": current_room.exits
+            }
+        self.last_options = options
+        return options
 
-    def execute(self,actions: list[(str,str)]):
+    def execute(self,actions: list[data.Action]):
         for action in actions:
-            type,val = action
-            if type == "move_room":
-                self.player.current_room = self.player.current_room.exits[val]
-            elif type == "display_dialogue":
-                data.display(val)
-            elif type == "start_battle":
-                self.battle = Battle(self, self.player, val)
-
-            
+            if action.type == data.ActionType.MOVE:
+                self.player.current_room = data.room_dict[action.val]
+            elif action.type == data.ActionType.DISPLAY_DIA:
+                data.display(action.action_val)
+            elif action.type == data.ActionType.START_BATTLE:
+                self.battle = Battle(self, self.player, action.action_val)
+    
     def status(self):
         pass
+
+if __name__ == "__main__":
+    pass

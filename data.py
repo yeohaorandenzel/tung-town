8dca63f6695e6c8c53bc3418fe51e18cf9da9e3a
denzioed <denzioed@gmail.com>
2026-08-27 20:07:59 +0800

    more fixes

--- data.py
+++ data.py
@@ -2,18 +2,56 @@ class ActionType:
     MOVE = 1
     START_BATTLE = 2
     DISPLAY_DIA = 3
+    ATTACK = 4
+    HEAL = 5
+    SKIP = 6
 
 class Action:
-    def __init__(self,type:ActionType,val):
+    def __init__(self,type:ActionType,val,one_time = False):
         self.type = type
         self.val = val
+        self.one_time = one_time
+    def act_done(self):
+        if self.one_time:
+            self.type = ActionType.SKIP
 
 class Choice:
     def __init__(self,desc,actions):
         self.desc = desc
         self.actions = actions
 class Creature:
-    pass
+    def __init__(self,health,name,move_set:dict[str:int],max_health = 100):
+        self.health = health
+        self.name = name
+        self.move_set = move_set
+        self.max_health = max_health
+    def is_alive(self):
+        return self.health > 0
+    def take_damage(self,damage):
+        if damage >= 0:
+            self.health -= damage
+        else:
+            raise ValueError("Damage must be >= 0")
+    def heal(self, heal_amt):
+        if heal_amt >= 0:
+            self.health += heal_amt
+            if self.health > self.max_health:
+                self.health = self.max_health
+        else:
+            raise ValueError("Heal amount must be >= 0")
+    def act(self, action:Action,acted_on = None):
+        if action.type == ActionType.ATTACK:
+            acted_on.take_damage(action.val)
+        elif action.type == ActionType.HEAL:
+            self.heal(action.val)
+    def use_move(self, move_chosen, acted_on):
+        action = self.move_set[move_chosen]
+        self.act(action, acted_on)
+        if action.type == ActionType.ATTACK:
+            print(f'{self.name} attacks with {move_chosen} to deal {action.val} damage')
+        elif action.type == ActionType.HEAL:
+            print(f'{self.name} heals with {move_chosen} to gain {action.val} hp')
+
 class Room:
     def __init__(self, name: str, description: str, max_health: int,exits: dict[str, "Room"]):
         self.name = name
@@ -22,26 +60,54 @@ class Room:
         self.creatures: list[Creature] = []
         self.__max_health = max_health
 
-class Player:
-    def __init__(self, name: str,current_room):
-        self.name = name
+class Player(Creature):
+    def __init__(self, name: str,current_room,max_health,health,move_set):
+        super().__init__(health,name,move_set, max_health)
         self.current_room = current_room
+    
+
 def prompt_player_choice(choices_dict):
-    print(choices_dict["description"])
+    choice_disp_str = choices_dict["description"] + "\n"
     for choice_name,choice in choices_dict["choices"].items():
-        print(choice_name,choice.desc)
-    return input(">")
-def display(k):
-    pass
+        choice_disp_str += choice_name + '->' + choice.desc + '\n'
+    display(choice_disp_str)
+    valid = False
+    while not valid:
+        choice_made = input(">").lower()
+        if choice_made in choices_dict["choices"].keys():
+            valid = True
+        else:
+            print("Sorry?")
+    return choice_made
+def display(string,border = "-"):
+    print(border*90)
+    print(string)
+    print(border*90)
+def move_set_to_choices_table(move_set):
+    choices_table = {}
+    for k,v in move_set.items():
+        if v.type == ActionType.ATTACK:
+            choices_table[k] = Choice(f"Attack dealing {v.val} damage", v)
+        elif v.type == ActionType.HEAL:
+            choices_table[k] = Choice(f"Healing move that replenishes {v.val} hp", v)
+    return choices_table
+#Creatures
+BombCrocMoveset = {
+    "whip":Action(ActionType.ATTACK, 3),
+    "bash":Action(ActionType.ATTACK, 6)
+}
+BombCroc = Creature(70, "Bombardino Crocadilo",BombCrocMoveset,70)
 tung_town_case = Room(
     "TownSquare",
     "A desolate town square surrounded by faded grass. A demolished statue of tung tung tung sahur stands at the centre, heavy with strange fruit. Two rectangular creatures stand next to you.",
     100,
-    {"north": Choice("Scarlet Swamp", [Action(ActionType.MOVE, "scarlet_swamp")])}
+    {"north": Choice("Proceed to Scarlet Swamp", [Action(ActionType.MOVE, "scarlet_swamp"),Action(ActionType.DISPLAY_DIA, "As you walk through some tall grass something lunges at you."),Action(ActionType.START_BATTLE, BombCroc, True)])}
 )
-
-scarlet_swamp = Room("ScarletSwamp","Swampy",100,{"south": Choice("Tung town",[Action(ActionType.MOVE, "tung_town_case")])})
-
+scarlet_swamp = Room("ScarletSwamp",
+                     "Swampy",
+                     100,
+                     {"south": Choice("Proceed to Tung town",[Action(ActionType.MOVE, "tung_town_case")])}
+)
 room_dict = {
     "tung_town_case": tung_town_case,
     "scarlet_swamp": scarlet_swamp
--- game.py
+++ game.py
@@ -1,28 +1,42 @@
 import data
-class Battle:
-    def __init__(self, player, enemy):
-        self.player = player
-        self.enemy = enemy
-        print("You engage "+ self.enemy.name)
+import random
+def start_battle(player,enemy):
+    data.display("You engage " + enemy.name)
+    while player.is_alive() and enemy.is_alive():
+        data.display(f"""
+        {enemy.name} glares you down
 
-    def get_options(self):
-        
-        options = {
-            "choices": None
-        }
+        your HP: {player.health}/{player.max_health}
+        {enemy.name}'s HP: {enemy.health}/{enemy.max_health}
+        """)
+        options_table = {
+            "description": f"Moves:",
+            "choices": data.move_set_to_choices_table(player.move_set)
+        }  
+        player_move = data.prompt_player_choice(options_table)
+        enemy_move = random.choice(list(enemy.move_set.keys()))
+        player.use_move(player_move, enemy)
+        enemy.use_move(enemy_move, player)
+    if player.is_alive():
+        data.display("You come out of the battle victorious")
+    else:
+        data.display("Oh god it hurts it really doe-")
+    
 class Game:
     def __init__(self):
         self.player = None
         self.battle = None
         self.last_options = None
     def is_gameover(self):
-        return False
+        return not self.player.is_alive()
     def welcome(self):
         print("Hallo lets play da tung town press any key to start")
         input()
     
     def add_player(self, player):
         self.player = player
+        initial_room = player.current_room
+        data.display(initial_room.name,"=")
     
     def get_actions(self, choice_name):
         return self.last_options["choices"][choice_name].actions
@@ -42,14 +56,20 @@ class Game:
     def execute(self,actions: list[data.Action]):
         for action in actions:
             if action.type == data.ActionType.MOVE:
+                room_to_move_to = data.room_dict[action.val]
+                data.display(room_to_move_to.name,"=")
                 self.player.current_room = data.room_dict[action.val]
             elif action.type == data.ActionType.DISPLAY_DIA:
-                data.display(action.action_val)
+                data.display(action.val)
             elif action.type == data.ActionType.START_BATTLE:
-                self.battle = Battle(self, self.player, action.action_val)
-    
-    def status(self):
-        pass
+                start_battle(self.player, action.val)
+            action.act_done()
+    def epilogue(self):
+        if self.player.is_alive():
+            print("You wake up in your bed. Well it must've been a dream, you think.")
+        else:
+            print("...")
+        
 
 if __name__ == "__main__":
     pass
